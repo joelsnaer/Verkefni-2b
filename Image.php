6cb0b905ab52f1796babdf10f3f2fb79fc7ed7ec
joelsnaer <joelsnaer1999@hotmail.com>
2017-01-29 21:28:37 +0000

    Verkefni 2b, Image klass, Ekki hugmynd hvað við áttum að gera

---
+++ Image.php
@@ -0,0 +1,49 @@
+<?php
+namespace File;
+/**
+ * image calssinn fyrir verkefnið
+ */
+ /**
+  * image calssinn fyrir verkefnið
+  */
+ class Mynd
+ {
+     protected $name;
+     protected $size;
+     protected $owner;
+     protected $path;
+     public function __construct($file){
+       $this -> name = $file['image']['name'];
+       $this -> size = $file['image']['size'];
+       $this -> owner = $owner;
+       $this -> path = "media\\" . $file['image']['name'];
+     }
+     public function getImageName(){
+       return $this -> name;
+     }
+     public function getOwnerName(){
+       return $this -> owner;
+     }
+     public function getImageSize(){
+       return $this -> size;
+     }
+     public function getPath(){
+       return $this -> path;
+     }
+     public function setImageName($newname){
+       $this -> name = $newname;
+     }
+     public function setPath($newpath){
+       $this -> path = $newpath;
+     }
+     public function setOwnerName($newowner){
+       $this -> owner = $newowner;
+     }
+     public function showImageSize(){
+       echo $size;
+     }
+     public function showImage(){
+       echo "<img src = \"" . $path . "\" >"
+     }
+ }
+?>
